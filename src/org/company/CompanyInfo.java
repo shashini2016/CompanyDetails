b0992ffa5ad7dd28182290ca860b486a6237400c
shashini2016 <gayathriramesh2014@gmail.com>
2022-08-28 16:03:06 +0530

    verify info

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,22 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+		// TODO Auto-generated method stub
+  System.out.println("Enter the companyName");
+	}
+private void companyId() {
+	// TODO Auto-generated method stub
+System.out.println("Enter the companyId");
+}
+private void companyAddress() {
+	// TODO Auto-generated method stub
+System.out.println("Enter the companyAddress");
+}
+ public static void main(String[] args) {
+	CompanyInfo CompanyDetails = new CompanyInfo();
+			CompanyDetails.companyName();
+	        CompanyDetails.companyId();
+	        CompanyDetails.companyAddress();
+}
+}
---
+++ src/org/company/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author gayat
+ *
+ */
+package org.company;
